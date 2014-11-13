5595f1bc4e392c16e58b29bceedf1eb69a78ec48
zen <zen@163.com>
2014-11-13 17:24:17 +0800

    start collective intelligence project

---
+++ creation/collective_intelligence.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+"""
+    We believe through interesting compute rules, mass of simple units can behave quite interestingly, so I'm here to experiment with my own thoughts(literal form here).
+    The important things is to find the common features of each unit, and using a smart rule to organize it.
+    The first thing is to define unit. Was it a letter, a word, a sentence, or a paragraph?
+    It's interesting that a letter is meaningless, except it has some phonetic meanings.
+    But when several letters were assembled together, it was used to refer to some concept of our life.
+    However a single word is too brief, thus words themselves combined into sentence to bear more meanings.
+    Words has connections between them, some words can be object/subject of another, and some words are close/opposite.
+    A well formed set of words can produce a beautiful form: A decent sentence.
+    Sentences as a higher-level unit, can also be combined. The relationship between sentences is that they may intended to revealing something,
+    or to present an imaginary graph, sometimes a dinamic graph. The community of sentences is called a paragraph.
+    As something are quite complicate, or a picture is grand, we need many paragraphes to do this job.
+    Relative paragraphes composed a meaningful entity, if it's short, it is called an article, and if it is long, it forms a book.
+    And you can describe the scenary or the principles from another angle, then you can write another book.
+    So books talking about similar topics together formed a knowledge system.
+    And knowledge system with connections together forms a huger system.
+    Until it reach its ultimate: The whole wisdom of the universe.
+
+    But be aware, as an artificial production, books are usually read by an linear order.
+    So the words, sentences, paragraphs which are closer usually have stronger correlations
+    Though maybe two paragraphs have close relationship, but two arbitrary sentences chosen from them could be totally irrelevant.
+    The same thing happens on sentence-word scale. Which means the relationship on higher organizations doesn't affect much about its lower organizations.
+    The element of lower organization has sort of independence.
+"""
+
+def core_rules(**paras):
+    pass
+
---
+++ creation/zen_dict/New_Spanish_dict.py
@@ -0,0 +1,210 @@
+# coding:utf-8
+import sys
+from Spanish_cache import raw_dict
+import datetime
+def a():
+    'short for add'
+    new_word = raw_input('Enter the word:')
+    if new_word in raw_dict:
+        pass    # do something here
+    else:
+        meaning = raw_input("Enter the Meaning below and press ENTER when finished typing:\n\n")
+        if not new_word:
+            return
+        type = raw_input('\nEnter the word type, please:\n')
+        raw_dict[new_word] = { 'meaning': meaning, 'synonym': [], 'english': '', 'relative_word':[],
+                'pronounciation':'', 'word_type': [], 'forget_score': 0, 'create_time': datetime.datetime.now()}
+    save()
+
+def wash_ass(target_dict):
+    for old_word in target_dict:
+        if type(raw_dict[old_word]) is list:
+            try:
+                if len(raw_dict[old_word]) == 7:
+                    meaning, synonym, english, forget_score, pronounciation, relative_word, word_type = raw_dict[old_word]
+                else:
+                    meaning, synonym, english, forget_score, pronounciation, relative_word, = raw_dict[old_word]
+            except:
+                print raw_dict[old_word]
+                sys.exit(0)
+            target_dict[old_word] = {}
+            target_dict[old_word]['meaning'] = meaning
+            target_dict[old_word]['synonym'] = synonym
+            target_dict[old_word]['english'] = english
+            target_dict[old_word]['forget_score'] = forget_score
+            target_dict[old_word]['pronounciation'] = pronounciation
+            target_dict[old_word]['relative_word'] = relative_word
+    save()
+
+def d():
+    'short for del'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        raw_dict.pop(old_word)    # do something here
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def t():
+    'short for type'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        word_type =  raw_input("Enter the Word_Type below and press ENTER when finished typing:\n\n")
+        try:
+            if word_type not in raw_dict[old_word].get('word_type'):
+                raw_dict[old_word].get('word_type').append(word_type)
+        except:
+            raw_dict[old_word]['word_type'] = [word_type]
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def g():
+    'short for get'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        meaning = raw_dict[old_word].get('meaning')
+        synonym = raw_dict[old_word].get('synonym')
+        english = raw_dict[old_word].get('english')
+        forget_score = raw_dict[old_word].get('forget_score')
+        pronounciation = raw_dict[old_word].get('pronounciation')
+        relative_word = raw_dict[old_word].get('relative_word')
+        word_type = raw_dict[old_word].get('word_type')
+        create_time = raw_dict[old_word].get('create_time')
+
+
+        print old_word
+        print 'meaning:', meaning
+        if synonym:
+            print 'synonym:', synonym
+        if english:
+            print 'english:', english
+        if forget_score:
+            print 'forget_score:', forget_score
+        if pronounciation:
+            print 'pronounciation:', convert(pronounciation)
+        if relative_word:
+            print 'relative_word:', relative_word
+        if word_type:
+            print 'word_type:', word_type
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+
+def convert(pronounciation):
+    return pronounciation.replace('Z','ð').replace('SH','ʃ').replace('S','s').replace('J','ʒ').replace('G','dʒ').replace('CH','tʃ').replace('E','ə').replace('A','ʌ').replace('O','ɔ').replace('N','ŋ')
+
+def e():
+    'short for español if you are in EnglishDict, short for english if you are in SpanishDict'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        english =  raw_input("Enter the Spanish below and press ENTER when finished typing:\n\n")
+        raw_dict[old_word]['english'] = english
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def m():
+    'short for meaning'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        if raw_dict[old_word].get('meaning'):
+            confirm = raw_input('The current meaning is: %s, press y to continue, press other to quit\n'  % raw_dict[old_word].get('meaning'))
+            if confirm == 'y':
+                meaning =  raw_input("Enter the meaning below and press ENTER when finished typing:\n\n")
+                raw_dict[old_word]['meaning'] = meaning
+        else:
+            meaning =  raw_input("Enter the meaning below and press ENTER when finished typing:\n\n")
+            raw_dict[old_word]['meaning'] = meaning
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def s():
+    'short for synonym'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        synonym =  raw_input("Enter the Synonym below and press ENTER when finished typing:\n\n")
+        raw_dict[old_word].get('synonym').append(synonym)
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def r():
+    'short for relative words'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        relative_word =  raw_input("Enter the Relative_Word below and press ENTER when finished typing:\n\n")
+        if relative_word not in raw_dict[old_word].get('relative_word'):
+            raw_dict[old_word].get('relative_word').append(relative_word)
+        if raw_dict.get(relative_word):
+            if old_word not in raw_dict[relative_word]['relative_word']:
+                raw_input[relative_word]['relative_word'].append(old_word)
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def p():
+    'short for pronounciation, for ENGLISH word only'
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        pronounciation =  raw_input("Enter the Pronounciation below and press ENTER when finished typing:\n\n")
+        raw_dict[old_word]['pronounciation'] = pronounciation
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def f(score=1,):
+    """short for forget, default score is 1, if you want to lower a word\'s socre, just pass negative number to this func"""
+    old_word = raw_input('Enter the word:')
+    if old_word in raw_dict:
+        raw_dict[old_word]['forget_score'] += score
+    else:
+        print " The word you are searching for is not in this dict now, please check your spelling."
+    save()
+
+def v(rev=True, SS=False, L2=False, T=False ):
+    """Short for view, default to rank from the word you forgot the most times, pass any value to rank from reverse
+    L2 = bilingual, SS = show score, rev = Reverse, T = by time order
+    """
+    if not SS:
+        for i in sorted(raw_dict.keys(), key=lambda x: raw_dict[x].get('create_time') if T else raw_dict[x].get('forget_score') , reverse = rev):
+            if L2:
+                if not raw_dict[i].get('english'):
+                    continue
+            print i
+    else:
+        for i in sorted(raw_dict.keys(), key=lambda x: raw_dict[x].get('create_time') if T else raw_dict[x].get('forget_score'), reverse = rev):
+            if L2:
+                if not raw_dict[i].get('english'):
+                    continue
+            print i, raw_dict[i].get('forget_score')
+
+def h():
+    """ show IPA(International Phonetic Alphabet) mapping """
+    print """
+              E for ə
+              A for ʌ
+              O for ɔ
+              J for ʒ
+             SH for ʃ
+              N for ŋ
+              S for θ
+              Z for ð
+             CH for tʃ
+              G for dʒ
+          """
+
+def save():
+    f = open('Spanish_cache.py', 'w')
+    f.write('raw_dict=%r' % raw_dict)
+    f.close()
+
+def se(quit_sig=False):
+    ' short for save exit, actually it is often used for backup'
+    f = open('Spanish_backup.py', 'a')
+    now = str(datetime.datetime.now())[:19]
+    f.write('\nlast_time=%r\n' % now)
+    f.write('raw_dict=%r\n' % raw_dict)
+    f.close()
+    if quit_sig:
+        sys.exit(0)
