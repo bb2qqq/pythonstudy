ccfe6de0ef3db09606524c0649e26602004c999d
zen <zen@163.com>
2014-12-03 10:31:38 +0800

    0

--- tech_lab/algorithm/deberes/test.py
+++ tech_lab/algorithm/deberes/test.py
@@ -6,71 +6,11 @@ def recur(my_dict):
     wr_n = 0
     try:
         for item in my_dict:
+            wr_n += 1
             recur(item)
     except:
         wr_n += 1
         pass
 
     if wr_n == len(my_dict.keys()):
-        yield result
-
-
-
-
-
-
-
-def get_dict_level(target):
-    n = 0
-    if target.keys():
-        n += 1
-        for i in target.keys():
-            if get_dict_level(i):
-                n += 1
-
-    print n
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return my_dict
