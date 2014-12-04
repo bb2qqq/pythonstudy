410f903844f584c6389e3ce538185a68c076cb85
zen <zen@163.com>
2014-12-04 12:31:01 +0800

    n

--- tech_lab/algorithm/deberes/test.py
+++ tech_lab/algorithm/deberes/test.py
@@ -1,27 +1,6 @@
-# what do we want: to say the deepest level of a dict
-# what do we have, all the elements of the dict
-
-
-def nested_depth(d):
-    if not isinstance(d, dict):
+def nested_depth(target):
+    if not isinstance(target, dict):
         return 0
-    if not d:
+    if not target:
         return 1
-    return 1 + max(nested_depth(v) for v in d.values())
-
-
-
-
-
-def recur(my_dict):
-    wr_n = 0
-    try:
-        for item in my_dict:
-            wr_n += 1
-            recur(item)
-    except:
-        wr_n += 1
-        pass
-
-    if wr_n == len(my_dict.keys()):
-        return my_dict
+    return 1 + max(nested_depth(v) for v in target.values())
