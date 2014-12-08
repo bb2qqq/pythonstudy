f99e86418e8ff7f5d5c49cb2cef45ae3786e61bf
zen <zen@163.com>
2014-12-08 14:43:32 +0800

    now the compositora is nearlly workable

--- creation/compositora.py
+++ creation/compositora.py
@@ -1,24 +1,31 @@
 from random import random, choice
-consonant = [k, w, r, "rr," t, p, s, z, d, f, g, h, j, l, m, n, b, v, "ch," "ll," "tr," "dr," "sh," ]
-vowel = [a, e, i, o, u,]
-suffix = [k, r, t, p, s, z, d, f, g, h, j, l, n, v, "ch," "sh,"]
+consonant = ['k', 'w', 'r', 'rr', 't', 'p', 's', 'z', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'b', 'v', 'ch', 'll', 'tr', 'dr', 'sh', ]
+vowel = ['a', 'e', 'i', 'o', 'u',]
+vowel_compound = [ 'an', 'en', 'in', 'on', 'un', 'ang', 'eng', 'ing', 'ong', 'ung', 'uai', 'ae', 'ai', 'ao', 'au', 'ea', 'ei', 'eo', 'eu', 'ia', 'ie', 'io', 'iu', 'oa', 'oe', 'oi', 'ou', 'ua', 'ue', 'ui', 'uo']
+suffix = ['k', 'r', 't', 'p', 's', 'z', 'd', 'f', 'g', 'h', 'j', 'l', 'n', 'v', 'ch', 'sh',]
 
 
-def m_word():
-    if random() < 0.5:
+def m_word(h_rate=0.2, b_rate=0.2, t_rate=0.8):
+    compound_sig = False
+    if random() < h_rate:
         H = ''
     else:
-        H = random.choice(consonant)
+        H = choice(consonant)
 
-    if random() < 0.5:
-        B = ''
+    if random() < b_rate:
+        B = choice(vowel_compound)
+        compound_sig = True
     else:
-        B = random.choice(vowel)
+        B = choice(vowel)
 
-    if random() < 0.5:
+    if not compound_sig:
+        if random() < t_rate:
+            T = ''
+        else:
+            T = choice(suffix)
+
+    else:
         T = ''
-    else:
-        T = random.choice(suffix)
 
     word = H + B + T
     return word
@@ -30,10 +37,10 @@ def generate(L):
             word = m_word()
             temp_L.append(word)
         temp_L = ' '.join(temp_L)
-    print temp_L
+        print temp_L
 
 
 
 test_L = [7, 8, 8, 8, 3, 3, 8, 3, 3, 7]
 
-generate(temp_L)
+generate(test_L)
